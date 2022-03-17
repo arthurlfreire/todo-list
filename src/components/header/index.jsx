281c3adc7c6c22e2ef5f9e872fab42843b79580b
arthurlfreire <98797665+arthurlfreire@users.noreply.github.com>
2022-03-17 19:36:14 -0300

    Create Header component

---
+++ src/components/header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./index.scss"
+
+export default function Header(){
+  return(
+    <header className="header">
+      <h1>Todo List</h1>
+      <h3>Create and storage your everyday todo list.</h3>
+    </header>
+  )
+}
